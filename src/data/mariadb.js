fd863a6d97a6ee7ea0584917fa487ff801401f29
ChanGyunLee <lgyn10@naver.com>
2024-05-21 09:24:48 +0900

    feat: mysql pwd 암호화

--- src/data/mariadb.js
+++ src/data/mariadb.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // 데이터베이스와 연동
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'root', // pwd 추가
+  password: process.env.MYSQL_PRIVATE_KEY, // pwd 추가
   database: 'BookShop',
   dateStrings: true, // 변경해줘야 time_zone 설정이 적용된 시간을 얻을 수 있다.
 });
