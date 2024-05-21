b19aee4391100aed75f3de71519320daff5cd656
ChanGyunLee <lgyn10@naver.com>
2024-05-21 09:13:52 +0900

    feat: app.js에서 모듈 절대경로로 불러오기

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 
-const userRouter = require('./routes/users');
-const bookRouter = require('./routes/books');
-const cartRouter = require('./routes/carts');
-const likeRouter = require('./routes/likes');
-const orderRouter = require('./routes/orders');
+const userRouter = require('routes/users');
+const bookRouter = require('routes/books');
+const cartRouter = require('routes/carts');
+const likeRouter = require('routes/likes');
+const orderRouter = require('routes/orders');
 
 app.use('/users', userRouter);
 app.use('/books', bookRouter);
