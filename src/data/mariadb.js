f100ba283df507cb6f67c48da57a45abd0ca04a8
ChanGyunLee <lgyn10@naver.com>
2024-05-21 09:14:14 +0900

    chore: db 명 변경

--- src/data/mariadb.js
+++ src/data/mariadb.js
@@ -1,12 +1,11 @@
-// Get the client
 const mysql = require('mysql2');
 
-// Create the connection to database
+// 데이터베이스와 연동
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root', // pwd 추가
-  database: 'Youtube',
+  database: 'BookShop',
   dateStrings: true, // 변경해줘야 time_zone 설정이 적용된 시간을 얻을 수 있다.
 });
 
