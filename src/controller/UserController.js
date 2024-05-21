4f9739f4cf6fbeedc8a6cf5ad8e58b49caa84315
ChanGyunLee <lgyn10@naver.com>
2024-05-21 13:49:21 +0900

    feat: 회원가입 응답 확인 로직 추가

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,13 +1,15 @@
 const conn = require('data/mariadb');
-// http-status-codes 라이브러리
+const { StatusCodes } = require('http-status-codes'); // http-status-codes 라이브러리
 const { StatusCodes } = require('http-status-codes');
 
+//! 회원가입
 const join = (req, res) => {
   const { email, password } = req.body;
   const sql = `insert into users (email, password) values ('${email}','${password}')`;
   conn.query(sql, (error, results) => {
-    if (error) {
-      return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
+    if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
+    if (results.affectedRows > 0) {
+      res.status(StatusCodes.CREATED).json(`${email}님, 회원가입에 성공했습니다!`);
     }
     res.status(StatusCodes.CREATED).json(`${email}님, 회원가입에 성공했습니다!`);
     console.log(results);
