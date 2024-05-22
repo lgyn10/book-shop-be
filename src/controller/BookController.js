c13bcab23cfca375732eca7416a73533799011c7
ChanGyunLee <lgyn10@naver.com>
2024-05-22 10:20:36 +0900

    feat: 도서 조회 API 구현

---
+++ src/controller/BookController.js
@@ -0,0 +1,46 @@
+const conn = require('data/mariadb');
+const { StatusCodes } = require('http-status-codes'); // http-status-codes 라이브러리
+require('dotenv').config(); // .env 파일 사용
+
+//! 전체 도서 조회
+const allBooks = (req, res) => {
+  const { categoryId } = req.query;
+  //| 카테고리별 도서 조회
+  if (categoryId) {
+    const sql = 'select * from books where category_id = ?';
+    const values = [categoryId];
+    conn.query(sql, values, (error, results) => {
+      if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+      if (results.length) {
+        return res.status(StatusCodes.OK).json(results);
+      } else {
+        return res.status(StatusCodes.NOT_FOUND).end();
+      }
+    });
+  } else {
+    // 요약된 도서 리스트
+    // const sql = 'select id, title, summary, author, price, pub_date from books';
+    const sql = 'select * from books';
+    conn.query(sql, (error, results) => {
+      if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+      return res.status(StatusCodes.OK).json(results);
+    });
+  }
+};
+
+//! 개별 도서 조회
+const bookDetail = (req, res) => {
+  const id = parseInt(req.params.id);
+  const sql = 'select * from books where id = ?';
+  const values = [id];
+  conn.query(sql, values, (error, results) => {
+    if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+    if (results.length) {
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end();
+    }
+  });
+};
+
+module.exports = { allBooks, bookDetail };
--- src/routes/books.js
+++ src/routes/books.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
 router.use(express.json());
 
+const { allBooks, bookDetail, booksByCategory } = require('controller/BookController');
+
 //! 전체 도서 조회
-router.get('/', (req, res) => {});
+//| +카테고리별 도서 조회
+router.get('/', allBooks);
 
 //! 개별 도서 조회
-router.get('/:id', (req, res) => {});
-
-//! 카테고리별 도서 조회
-router.get('/', (req, res) => {
-  const { categoryId, isNew } = req.query;
-  res.json({ categoryId, isNew });
-});
+router.get('/:id', bookDetail);
 
 module.exports = router;
