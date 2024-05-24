a776ed0683a902c81b84b8be0da386ded776f2d8
ChanGyunLee <lgyn10@naver.com>
2024-05-24 22:05:13 +0900

    feat: 쿼리 파라미터 값 형 변환

--- src/controller/BookController.js
+++ src/controller/BookController.js
@@ -4,20 +4,20 @@ require('dotenv').config(); // .env 파일 사용
 
 //! 전체 도서 조회
 const allBooks = (req, res) => {
-  const { categoryId } = req.query;
-  //| 카테고리별 도서 조회
-  if (categoryId) {
-    const sql = 'select * from books where category_id = ?';
-    const values = [categoryId];
-    conn.query(sql, values, (error, results) => {
-      if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error });
-      if (results.length) {
-        return res.status(StatusCodes.OK).json(results);
-      } else {
-        return res.status(StatusCodes.NOT_FOUND).end();
-      }
-    });
-  } else {
+  const { categoryId, news, limit, currentPage } = req.query; // 존재 안하면 [undefined]
+  // limit : page 당 도서 수
+  // currentPage : 현재 몇 페이지
+  // offset : limit * (currentPage - 1)
+
+  // string -> number 형 변환
+  const parsedLimit = parseInt(limit, 10); // NaN or value
+  const parsedCurPage = parseInt(currentPage, 10);
+  const offset = parsedLimit * (parsedCurPage - 1);
+  const parsedcategoryId = parseInt(categoryId, 10);
+
+  // news : string -> boolean 형 변환
+  const parsedNews = JSON.parse(news ?? false);
+
     // 요약된 도서 리스트
     // const sql = 'select id, title, summary, author, price, pub_date from books';
     const sql = 'select * from books';
