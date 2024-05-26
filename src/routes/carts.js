57b26622051ac4bbd54049096900aa66b87cbd16
ChanGyunLee <lgyn10@naver.com>
2024-05-26 15:02:08 +0900

    feat: cart.js에서 컨트롤러 모듈화

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -1,18 +1,19 @@
+const { addCartItem, getCartItems, deleteCartItem, getSelectedCartItems } = require('controller/CartController');
 const express = require('express');
 const router = express.Router();
 
 router.use(express.json());
 
-//! 장바구니 담기
-router.post('/', (req, res) => {});
+//! 장바구니 담기(추가)
+router.post('/', addCartItem);
 
-//! 장바구니 조회
-router.get('/', (req, res) => {});
+//! 장바구니 도서 전체 조회
+router.get('/', getCartItems);
 
-//! 장바구니 도서 삭제
-router.delete('/:id', (req, res) => {});
+//! 장바구니 도서 개별 삭제
+router.delete('/:cartItemId', deleteCartItem);
 
 //! 장바구니에서 선택한 주문 상품 목록 조회
-router.get('/', (req, res) => {});
+router.get('/', getSelectedCartItems);
 
 module.exports = router;
