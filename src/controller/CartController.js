729b6ddb9d62fe94dda26a60fd2f91dd9bccd9ed
ChanGyunLee <lgyn10@naver.com>
2024-05-27 10:04:22 +0900

    feat: 장바구니에서 선택한 도서 목록 조회 API 구현

--- src/controller/CartController.js
+++ src/controller/CartController.js
@@ -18,16 +18,23 @@ const addCartItem = (req, res) => {
   });
 };
 
-//! 장바구니 도서 전체 조회
+//! 장바구니 도서 전체 조회 + 장바구니에서 선택한 도서 조회
 const getCartItems = (req, res) => {
-  const { userId } = req.body;
+  const { userId, selected } = req.body;
   const parsedUserId = parseInt(userId, 10);
+  const parsedSelected = selected.map(Number);
 
-  const sql = `SELECT b.*, ci.id as cart_items_id, ci.quantity
+  let sql = `SELECT b.*, ci.id as cart_items_id, ci.quantity
   FROM books AS b
   LEFT JOIN cart_items AS ci ON b.id = ci.book_id
-  WHERE ci.user_id = ?;`;
-  const values = [parsedUserId];
+  WHERE ci.user_id = ?`;
+  let values = [parsedUserId];
+
+  // 장바구니에서 선택한 도서 조회
+  if (selected.length > 0) {
+    sql += ` AND ci.id IN (?);`;
+    values = [...values, parsedSelected];
+  }
 
   conn.query(sql, values, (error, results) => {
     if (error) return res.status(StatusCodes.BAD_REQUEST).json({ message: error });
@@ -47,7 +54,4 @@ const deleteCartItem = (req, res) => {
   });
 };
 
-//! 장바구니에서 선택한 주문 도서 목록 조회
-const getSelectedCartItems = (req, res) => {};
-
-module.exports = { addCartItem, getCartItems, deleteCartItem, getSelectedCartItems };
+module.exports = { addCartItem, getCartItems, deleteCartItem };
--- src/routes/carts.js
+++ src/routes/carts.js
@@ -1,4 +1,4 @@
-const { addCartItem, getCartItems, deleteCartItem, getSelectedCartItems } = require('controller/CartController');
+const { addCartItem, getCartItems, deleteCartItem } = require('controller/CartController');
 const express = require('express');
 const router = express.Router();
 
@@ -7,13 +7,10 @@ router.use(express.json());
 //! 장바구니 담기(추가)
 router.post('/', addCartItem);
 
-//! 장바구니 도서 전체 조회
+//! 장바구니 도서 전체 조회 + 장바구니에서 선택한 도서 조회
 router.get('/', getCartItems);
 
 //! 장바구니 도서 개별 삭제
 router.delete('/:cartItemId', deleteCartItem);
-
-//! 장바구니에서 선택한 주문 상품 목록 조회
-router.get('/', getSelectedCartItems);
 
 module.exports = router;
