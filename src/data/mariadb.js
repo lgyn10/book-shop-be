0cea2535b5f2e10817e3edcf1a05ee34417be778
ChanGyunLee <lgyn10@naver.com>
2024-06-03 19:59:51 +0900

    feat: 프로미스 모듈 제거, 원복

--- src/data/mariadb.js
+++ src/data/mariadb.js
@@ -1,16 +1,13 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 // 데이터베이스와 연동
-const connection = async () => {
-  const conn = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.MYSQL_PRIVATE_KEY, // pwd 추가
-    database: 'BookShop',
-    dateStrings: true, // 변경해줘야 time_zone 설정이 적용된 시간을 얻을 수 있다.
-  });
-  return conn;
-};
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: process.env.MYSQL_PRIVATE_KEY, // pwd 추가
+  database: 'BookShop',
+  dateStrings: true, // 변경해줘야 time_zone 설정이 적용된 시간을 얻을 수 있다.
+});
 
 module.exports = connection;
